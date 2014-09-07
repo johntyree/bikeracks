3d2fca0863058e01a1afc8cc836a9d61aa597f17
John Tyree <johntyree@gmail.com>
2014-09-07 03:06:27 -0500

    Extend takeAPhoto func to take a callback

--- js/Utils.js
+++ js/Utils.js
@@ -1,8 +1,28 @@
 var Utils = (function($) {
     return {
-        takeAPhoto: function takeAPhoto() {
+        takeAPhoto: function takeAPhoto(id, callback) {
+            console.log('start');
             var photoChooser = $('#photoChooser');
             photoChooser.click();
+            photoChooser.on('change', function() {
+                console.log('change fired');
+                var file = photoChooser[0].files[0];
+                console.log(file);
+                var reader = new FileReader();
+                reader.onload = function() {
+                    console.log('loaded');
+                    var imgdiv = $('#rack-img-' + id);
+                    var img = imgdiv.children('img');
+                    imgdiv.removeClass('hidden');
+                    img.attr('src', reader.result);
+                    dataURL = reader.result;
+                    callback && callback(reader.result);
+                };
+                reader.onerror = function() {
+                    alert('Error uploading file!');
+                };
+                reader.readAsDataURL(file);
+            });
         }
     };
 }(jQuery));
