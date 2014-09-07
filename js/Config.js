e0359c64245e50a18ab4ed048f690aca72da93e7
John Tyree <johntyree@gmail.com>
2014-09-07 03:03:08 -0500

    Add bikeracks and Imgur API params to config

--- js/Config.js
+++ js/Config.js
@@ -1,27 +1,37 @@
+var Config = (function() {
 
-var Config = {
+    var rootURL = 'http://john.bitsurge.net/bikeracks';
+    var imageURL = '/static/images';
+    return {
 
-    // API
-    nearbyRacksURL: 'static/data/austin_racks_v1.json',
+        // API
+        nearbyRacksURL: '/static/data/austin_racks_v1.json',
+        updateRackURL: rootURL + '/cgi-bin/bikeracks.py',
+        getRackURL: rootURL + '/get/',
 
-    rackIconOptions: {
-        iconUrl: 'static/images/parking_bicycle_0.png',
-        shadowUrl: 'static/images/parking_bicycle_shadow_0.png',
-        clusterIconUrl: 'static/images/parking_bicycle_cluster_0.png',
-        clusterShadowUrl: 'static/images/parking_bicycle_cluster_shadow_0.png',
-        iconAnchor: [13, 32],
-        popupAnchor: [5, -24]
-    },
+        // Imgur API
+        imgurClientID: 'f962410f5a6a13d',
 
-    markerClusterOptions: {
-        maxClusterRadius: 30
-    },
+        rackIconOptions: {
+            iconUrl: imageURL + '/parking_bicycle_0.png',
+            shadowUrl: imageURL + '/parking_bicycle_shadow_0.png',
+            clusterIconUrl: imageURL + '/parking_bicycle_cluster_0.png',
+            clusterShadowUrl:
+                imageURL + '/parking_bicycle_cluster_shadow_0.png',
+            iconAnchor: [13, 32],
+            popupAnchor: [5, -24]
+        },
 
-    geoOptions: {
-        enableHighAccuracy: true
-    },
+        markerClusterOptions: {
+            maxClusterRadius: 30
+        },
 
-    // Map view config
-    defaultZoom: 14
+        geoOptions: {
+            enableHighAccuracy: true
+        },
 
-};
+        // Map view config
+        defaultZoom: 14
+
+    };
+})();
